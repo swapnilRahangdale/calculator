0691f6658a72ed26860d12831c00c117a3c9c96c
swapnilRahangdale <rahangdaleswapnil7030@gmail.com>
2023-05-22 21:42:44 +0530

    frist cal commit

---
+++ src/components/Cal/Cal.js
@@ -0,0 +1,26 @@
+import {useState} from "react";
+import CalWrapper from "../CalWrapper/CalWrapper";
+
+const Cal = () => {
+
+  const [inputVal1, setInputVal1] = useState(0);
+
+    const allBtArr = [
+        
+        {value : 0, btFun: () => {setInputVal1(0)}},
+        {value : 1, btFun: () => {setInputVal1(1)}},
+        {value : 2, btFun: () => {setInputVal1(2)}},
+        {value : 3, btFun: () => {setInputVal1(3)}},
+        {value : 4, btFun: () => {setInputVal1(4)}},
+        {value : 5, btFun: () => {setInputVal1(5)}}
+    ];
+
+  return (
+    <div>
+      <p>This is calculator.    Enter No. {inputVal1}</p>
+        <CalWrapper allBtArr = {allBtArr}/>
+    </div>
+  );
+};
+
+export default Cal;
